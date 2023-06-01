73a1c7c6c0a97a2e395be682ddaf1127f5c93c07
arefinnuman <arefinnuman2000@gmail.com>
2023-06-02 02:44:46 +0600

    Winston Logger Added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './app/config'
+import { logger } from './app/config/logger'
 
 const usingPort = config.port
 const dataBaseUrl = config.database_url as string
@@ -8,12 +9,13 @@ const dataBaseUrl = config.database_url as string
 async function bootstrap() {
   try {
     await mongoose.connect(dataBaseUrl)
-    // console.log(`DataBase Connected`)
+    logger.info(`DataBase Connected`)
+
     app.listen(usingPort, () => {
-      // console.log(`EasyBuy Server on port ${usingPort}`)
+      logger.info(`EasyBuy Server on port ${usingPort}`)
     })
   } catch (err) {
-    // console.log(`Failed to connect`, err)
+    logger.error(`Failed to connect`, err)
   }
 }
 
